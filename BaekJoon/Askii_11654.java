2ab0b26623cfa877aa8c753654ed37f894ff5739
Jaeyoung Lee <leejaengjaeng@naver.com>
2016-11-28 21:50:12 +0900

    2016-11-28

---
+++ BaekJoon/Askii_11654.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Askii_11654
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		char input = sc.nextLine().charAt(0);
+		System.out.println((int)input);
+	}
+	
+}
